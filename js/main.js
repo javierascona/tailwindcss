240ef3d9130b05ea48e7a1068fe9f96505c165f7
javierascona <79239373+javierascona@users.noreply.github.com>
2021-05-18 23:59:49 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,57 @@
+
+const boton = document.querySelector('#boton');
+const menu = document.querySelector('#menu');
+
+boton.addEventListener('click', () => {
+    menu.classList.toggle('hidden')
+})
+
+
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 1,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 20
+        },
+        // when window width is >= 480px
+        480: {
+            slidesPerView: 3,
+            spaceBetween: 30
+        },
+        // when window width is >= 640px
+        640: {
+            slidesPerView: 3,
+            spaceBetween: 40
+        },
+
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 40
+        }
+        ,
+
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 40
+        }
+    }
+});
